0990077c1cbe74d618ee88a819b828c390db5042
sruthisruthi18112007 <sruthisruthi18112007@gmail.com>
2026-02-10 02:44:34 -0800

    Add files via upload

---
+++ practice/Add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+  class Add{
+   public static void main(String arr[]){
+     Scanner sc=new Scanner(System.in);
+  System.out.print("enter first number:");
+  int a = sc.nextInt();
+
+  System.out.print("enter second number:");
+  int b = sc.nextInt();
+
+ int sum = a+b;
+ System.out.println("Sum="+sum);
+}
+}
